8f65ae1534cd4d517a3f341bcfd9e06e71aa149a
Neha Kotarwar <nehak6296@gmail.com>
2021-07-16 16:26:50 +0530

    [Neha]Added:message on master branch

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -7,6 +7,8 @@ namespace Practice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Neha - this message is from master branch!");
+
         }
     }
 }
